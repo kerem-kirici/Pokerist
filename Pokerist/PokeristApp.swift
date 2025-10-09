198ca67328b9fb9a2bb550cc1cb39d8d5b939e66
kerem-kirici <kerem.kirici36@gmail.com>
2025-10-09 20:00:29 +0300

    Initial Commit

---
+++ Pokerist/PokeristApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeristApp.swift
+//  Pokerist
+//
+//  Created by Kerem Kirici on 9.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct PokeristApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokeristUITests/PokeristUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PokeristUITestsLaunchTests.swift
+//  PokeristUITests
+//
+//  Created by Kerem Kirici on 9.10.2025.
+//
+
+import XCTest
+
+final class PokeristUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
